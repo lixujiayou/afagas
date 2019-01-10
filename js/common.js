236599f8535f935b69f6d7ede90bc7759024dc57
李玉杰 <m15501129979@163.com>
2019-01-10 16:16:44 +0800

    lx-0110-1

--- js/common.js
+++ js/common.js
@@ -1,42 +1,33 @@
-﻿(function(w){
+(function(w){
 // 空函数
 function shield(){
 	return false;
 }
-document.addEventListener('touchstart',shield,false);//取消浏览器的所有事件，使得active的样式在手机上正常生效
+document.addEventListener('touchstart', shield, false);//取消浏览器的所有事件，使得active的样式在手机上正常生效
 document.oncontextmenu=shield;//屏蔽选择函数
 // H5 plus事件处理
 var ws=null,as='pop-in';
 function plusReady(){
 	ws=plus.webview.currentWebview();
-	// Android处理返回键
-	plus.key.addEventListener('backbutton',function(){
+	plus.key.addEventListener('backbutton', function(){
 		back();
 	},false);
-	compatibleAdjust();
 }
 if(w.plus){
 	plusReady();
 }else{
-	document.addEventListener('plusready',plusReady,false);
+	document.addEventListener('plusready', plusReady, false);
 }
 // DOMContentLoaded事件处理
-var domready=false;
-document.addEventListener('DOMContentLoaded',function(){
-	domready=true;
+document.addEventListener('DOMContentLoaded', function(){
 	gInit();
 	document.body.onselectstart=shield;
-	compatibleAdjust();
 },false);
-// 处理返回事件
+// 返回
 w.back=function(hide){
 	if(w.plus){
 		ws||(ws=plus.webview.currentWebview());
-		if(hide||ws.preate){
-			ws.hide('auto');
-		}else{
-			ws.close('auto');
-		}
+		(hide||ws.preate)?ws.hide('auto'):ws.close('auto');
 	}else if(history.length>1){
 		history.back();
 	}else{
@@ -44,82 +35,83 @@ w.back=function(hide){
 	}
 };
 // 处理点击事件
-var openw=null,waiting=null;
+var openw=null;
 /**
  * 打开新窗口
  * @param {URIString} id : 要打开页面url
- * @param {boolean} wa : 是否显示等待框
- * @param {boolean} ns : 是否不自动显示
+ * @param {String} t : 页面标题名称
  * @param {JSON} ws : Webview窗口属性
  */
-w.clicked=function(id,wa,ns,ws){
+w.clicked=function(id, t, ws){
 	if(openw){//避免多次打开同一个页面
 		return null;
 	}
 	if(w.plus){
-		wa&&(waiting=plus.nativeUI.showWaiting());
 		ws=ws||{};
 		ws.scrollIndicator||(ws.scrollIndicator='none');
 		ws.scalable||(ws.scalable=false);
-		var pre='';//'http://192.168.1.178:8080/h5/';
-		openw=plus.webview.create(pre+id,id,ws);
-		ns||openw.addEventListener('loaded',function(){//页面加载完成后才显示
-//		setTimeout(function(){//延后显示可避免低端机上动画时白屏
+		ws.backButtonAutoControl||(ws.backButtonAutoControl='close');
+		ws.titleNView=ws.titleNView||{};
+		ws.titleNView.autoBackButton = true;
+		ws.titleNView.backgroundColor = '#D74B28';
+		ws.titleNView.titleColor = '#CCCCCC';
+		ws.doc&&(ws.titleNView.buttons=ws.titleNView.buttons||[],ws.titleNView.buttons.push({fontSrc:'_www/helloh5.ttf',text:'\ue301',fontSize:'20px',onclick:'javascript:openDoc()'}));
+		t&&(ws.titleNView.titleText=t);
+		openw = plus.webview.create(id, id, ws);
+		openw.addEventListener('loaded', function(){
 			openw.show(as);
-			closeWaiting();
-//		},200);
-		},false);
-		openw.addEventListener('close',function(){//页面关闭后可再次打开
+		}, false);
+		openw.addEventListener('close', function(){
 			openw=null;
-		},false);
+		}, false);
 		return openw;
 	}else{
 		w.open(id);
 	}
 	return null;
 };
-w.openDoc=function(t,c){
-	var d=plus.webview.getWebviewById('document');
-	if(d){
-		d.evalJS('updateDoc("'+t+'","'+c+'")');
-	}else{
-		d=plus.webview.create('/plus/doc.html','document',{zindex:9999,popGesture:'hide'},{preate:true});
-		d.addEventListener('loaded',function(){
-			d.evalJS('updateDoc("'+t+'","'+c+'")');
-		},false);
-	}
-}
 /**
- * 关闭等待框
+ * 创建新窗口（无原始标题栏），
+ * @param {URIString} id : 要打开页面url
+ * @param {JSON} ws : Webview窗口属性
  */
-w.closeWaiting=function(){
-	waiting&&waiting.close();
-	waiting=null;
-}
-// 兼容性样式调整
-var adjust=false;
-function compatibleAdjust(){
-	if(adjust||!w.plus||!domready){
-		return;
-	}	// iOS平台使用滚动的div
-	if('iOS'==plus.os.name){
-		var t=document.getElementById("dcontent");
-		t&&(t.className="sdcontent");
-		t=document.getElementById("content");
-		t&&(t.className="scontent");
-		//iOS8横竖屏切换div不更新滚动问题
-		var lasto=window.orientation;
-		window.addEventListener("orientationchange",function(){
-			var nowo=window.orientation;
-			if(lasto!=nowo&&(90==nowo||-90==nowo)){
-				window.dcontent&&(0==dcontent.scrollTop)&&(dcontent.scrollTop=1);
-				window.content&&(0==content.scrollTop)&&(content.scrollTop=1);
-			}
-			lasto=nowo;
-		},false);
+w.createWithoutTitle=function(id, ws){
+	if(openw){//避免多次打开同一个页面
+		return null;
 	}
-	adjust=true;
+	if(w.plus){
+		ws=ws||{};
+		ws.scrollIndicator||(ws.scrollIndicator='none');
+		ws.scalable||(ws.scalable=false);
+		ws.backButtonAutoControl||(ws.backButtonAutoControl='close');
+		openw = plus.webview.create(id, id, ws);
+		openw.addEventListener('close', function(){
+			openw=null;
+		}, false);
+		return openw;
+	}else{
+		w.open(id);
+	}
+	return null;
 };
+/**
+ * 打开文档页面
+ * @param {URIString} c : 要打开页面url
+ */
+w.openDoc=function(c){
+	plus.webview.create(c, 'document', {
+		titleNView:{
+			autoBackButton:true,
+			backgroundColor:'#D74B28',
+			titleColor:'#CCCCCC'
+		},
+		backButtonAutoControl:'close',
+		scalable:false
+	}).show('pop-in');
+};
+/**
+ * 兼容提示
+ */
 w.compatibleConfirm=function(){
 	plus.nativeUI.confirm('本OS原生层面不提供该控件，需使用mui框架实现类似效果。请点击“确定”下载Hello mui示例',function(e){
 		if(0==e.index){
@@ -128,10 +120,9 @@ w.compatibleConfirm=function(){
 	},"",["确定","取消"]);
 }
 // 通用元素对象
-var _dout_=null,_dcontent_=null;
+var _dout_=null;
 w.gInit=function(){
 	_dout_=document.getElementById("output");
-	_dcontent_=document.getElementById("dcontent");
 };
 // 清空输出内容
 w.outClean=function(){
@@ -140,11 +131,13 @@ w.outClean=function(){
 };
 // 输出内容
 w.outSet=function(s){
+	console.log(s);
 	_dout_.innerText=s+"\n";
 	(0==_dout_.scrollTop)&&(_dout_.scrollTop=1);//在iOS8存在不滚动的现象
 };
 // 输出行内容
 w.outLine=function(s){
+	console.log(s);
 	_dout_.innerText+=s+"\n";
 	(0==_dout_.scrollTop)&&(_dout_.scrollTop=1);//在iOS8存在不滚动的现象
 };
@@ -179,6 +172,7 @@ w.ultZeroize=function(v,l){
 };
 })(window);
 
+// fast click 
 ;(function () {
 	'use strict';
 
